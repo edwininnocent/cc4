bfd631560d0a6f0d36c6e6a49587bdb6e62daf8e
edwininnocent <edwininnocent387@gmail.com>
2024-09-09 12:46:38 +0530

    Update cgpa.java

--- cgpa.java
+++ cgpa.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CGPACalculator {
+public class cgpa {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double[] marks = new double[4];
